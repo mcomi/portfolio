e822489924572d78255882eb74ab9348e4410b20
mcomix <mcomi@hotmail.com>
2021-08-31 18:13:58 -0500

    added custom 404 page

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
       </Head>
       <Layout>
         <div className="items-start max-w-2xl mx-auto mb-16">
-          <div className="w-32 lg:w-56 mx-auto">
+          <div className="hidden md:block md:w-64 mx-auto">
             <Image src={icon}></Image>
           </div>
           <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
